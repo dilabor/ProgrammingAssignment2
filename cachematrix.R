511c38781fefae27c31f3527762932bca39a0af2
animadverto <hroth@tngi.com>
2015-01-25 18:55:09 -0800

    First Commit
    Assignment 2 - Coursera R course

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## These are two functions that take advantage of lexical scoping to cache 
+## matrix data to improve performance by not computing the matrix inverse
+## if it has already been done and verifies the related matrix has not changed.
+##
+##
+## This function creates a list of functions to be used by a different
+## function in a separate environment allowing for the following:
+##    1. set the value of the matrix if it has not changed.
+##    2. get the value of the current matrix
+##    3. set the cached value of the inverse of the matrix
+##    4. get the value of the cached inverse of the matrix
+##   
+makeCacheMatrix <- function(x = matrix()) { 
+  matrix_inverse = NULL
+  set = function(y) {
+    ##
+    ##  Matrix check - set if different than previous updates else skips so 
+    ##  cached value if inverse is used and not recalculated
+    ##
+      if (!(identical(x,y)) {
+      x <<- y
+      matrix_inverse <<- NULL
+        }
+  }
+  get = function() x
+  setinverse = function(inverse) matrix_inverse <<- inverse 
+  getinverse = function() matrix_inverse
+  list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
-
-
-## Write a short comment describing this function
+ 
+## The following function sees if the previous function has cached the matrix
+## inverse.  If so it diplays an appropriate message and returns the cahced value.
+## If there is no cached value then the function computes the inverse matric and 
+## caches the result before returning it.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+    matrix_inverse =  x$getinverse()
+    if (!is.null(matrix_inverse)){
+      message ("Getting cached data...")
+      return(matrix_inverse)
+    }
+    matrix_data = x$get()
+    matrix_inverse = solve(matrix_data, ...)
+    x$setinverse(matrix_inverse)
+    return(matrix_inverse)
+  }
+   
